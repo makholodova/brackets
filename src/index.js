7fd6580f88913899acfc83203e60aa56e4fbd555
Marina Kholodova <marinaholodova@gmail.com>
2024-09-04 11:41:51 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	console.log(`str: ${str}`)
+	console.log(`bracketsConfig: ${bracketsConfig}`)
+
+	const obj = Object.fromEntries(bracketsConfig);
+	const bracketsObj = Object.fromEntries(Object.entries(obj).map(([key, value]) => [value, key]));
+
+	const openBrackets = [];
+	for (let i = 0; i < bracketsConfig.length; i++) {
+		openBrackets.push(bracketsConfig[i][0])
+	}
+	
+	let stack = [];
+	for (let i = 0; i < str.length; i++) {
+		let currentBracket = str[i];
+		
+		if (openBrackets.includes(currentBracket)) {
+			if (stack.length > 0 && stack[stack.length - 1] === currentBracket && bracketsObj[currentBracket] === currentBracket) {
+				stack.pop();
+			} else {
+				stack.push(currentBracket); 
+			}
+
+		} else {
+			if (stack[stack.length - 1] === bracketsObj[currentBracket]) {
+				stack.pop();
+			} else {
+				return false;
+			}
+		}
+	}
+
+	return stack.length === 0;
 }
